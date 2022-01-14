39c7eee20cd874f60c50c97244584b5abd2a5953
crogamer99 <crogamer99@gmail.com>
2022-01-14 19:29:15 +0100

    adding tas 2

---
+++ 14-01-22/vjezba2/pocetno.js
@@ -0,0 +1,8 @@
+var broj1 = 3;
+var broj2 = 7;
+
+console.log("broj1 je type:",typeof(broj1)); //provjera za broj1
+console.log("broj2 je type:",typeof(broj2)); //provjera za broj2
+
+console.log(broj1==broj2); //ne provjerava datatype
+console.log(broj1===broj2); //provjerava datatype
