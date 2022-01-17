67ef87ae599dac2fea83f0a4307296711c489a3f
crogamer99 <crogamer99@gmail.com>
2022-01-17 18:46:16 +0100

    fixing mistakes

--- 17-01-22/vjezba1/script.js
+++ 17-01-22/vjezba1/script.js
@@ -3,12 +3,14 @@ var textLength, textSit, textPlus;
 
 textLength = text.length; // 1)
 
+textSit = text.substring(19, 22); // 2)
+
 text = text.replace("amet", "elit"); //3)
 
 textPlus = (text + ", consectetur adipiscing elit"); //4)
 
 console.log(text.toUpperCase()); //5)
 
-console.log(text.trimStart()); //6)
+console.log(text.trim()); //6)
 
-console.log(text.charAt(11)); //7)
+console.log(text.charAt(12)); //7)
