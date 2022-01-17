85183b1749ee45fe7205e5f5e2e850c666006621
crogamer99 <crogamer99@gmail.com>
2022-01-17 18:30:57 +0100

    adding some tasks

---
+++ 17-01-22/vjezba1/script.js
@@ -0,0 +1,14 @@
+var text = " Lorem ipsum dolor sit amet";
+var textLength, textSit, textPlus, text1;
+
+textLength = text.length; // 1)
+
+text = text.replace("amet", "elit"); //3)
+
+textPlus = (text + ", consectetur adipiscing elit"); //4)
+
+console.log(text.toUpperCase()); //5)
+
+console.log(text.trimStart()); //6)
+
+console.log(text.charAt(11)); //7)
