2c9a0da98d2a1ed13ac46195bc7e73bdbba81d8e
crogamer99 <crogamer99@gmail.com>
2022-01-19 19:23:06 +0100

    adding tasks 2

---
+++ 19-01-22/vjezba2/script.js
@@ -0,0 +1,12 @@
+var names = ["Ivana", "Sanjin", "Marijan", "Doroti", "Damit", "Lorena", "Ružica"];
+
+names.forEach(Element => console.log(Element));
+
+var nizovi =[
+    ["Dijete", "Odrasla osoba"],
+    ["Računalo", "Pisača mašina"],
+    ["Mokro", "Suho"]
+];
+console.table(nizovi);
+nizovi.push(["Gladan", "Sit"]);
+console.table(nizovi);
