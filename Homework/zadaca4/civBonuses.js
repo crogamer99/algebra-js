d0e9f16742b04ee9ac212687191b035d446be568
crogamer99 <crogamer99@gmail.com>
2022-02-09 12:54:32 +0100

    Update

--- Homework/zadaca4/civBonuses.js
+++ Homework/zadaca4/civBonuses.js
@@ -2,5 +2,14 @@ const url = "https://age-of-empires-2-api.herokuapp.com/api/v1/civilizations";
 fetch(url)
 .then(response => response.json())
 .then(async (data) =>{
-        document.getElementById(`civName1`).innerHTML = `${data.civilizations[0].name}`,
-        document.getElementById(`civBonus1`).innerHTML = `${data.civilizations[0].civilization_bonus}`})
+        for(let i=0;i<32;++i){
+                g = document.createElement("p"),
+                g.setAttribute("id", "civName" + i),
+                document.getElementById("civBonus").appendChild(g),
+                h = document.createElement("p"),
+                h.setAttribute("id", "civBonus" + i),
+                document.getElementById("civBonus").appendChild(h),
+                document.getElementById(`civName` + i).innerHTML = `${data.civilizations[i].name}`,
+                document.getElementById(`civBonus` + i).innerHTML = `${data.civilizations[i].civilization_bonus} <br><hr>`;
+        }
+});
