776fa5ea1b761bbbf25ad98cd8a03bca9b7a8d10
crogamer99 <crogamer99@gmail.com>
2022-02-14 19:16:55 +0100

    adding tasks

---
+++ 14-02-22/vjezba1/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $("#hide").click(function(){
+      $("h1").hide();
+    });
+    $("#show").click(function(){
+      $("h1").show();
+    });
+    $(".enter").mouseover(function(){
+      $("body").css("background-color","blue");
+    })
+  });
